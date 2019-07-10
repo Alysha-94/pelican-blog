30d974a2a27ab55773fa891a6e1ddcc3cad2794a
Alysha-94 <alysha.iannetta@gmail.com>
2019-07-10 11:05:30 +1000

    pelican-quickstart blog skeleton

---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Alysha Iannetta'
+SITENAME = "Alysha's Blog"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Australia/Melbourne'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('Linkedin', 'https://www.linkedin.com/in/alysha-iannetta/'),
+          ('GitHub', 'https://github.com/Alysha-94'),
+          ('Send an email', 'mailto:alysha.iannetta@gmail.com'),)
+
+# THEME = 'custom'
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
